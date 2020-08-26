82bcce4837d90dd588dcb68f77a7c47e56a90a94
Paul H Bartley <paul.h.bartley@gmail.com>
2020-08-26 07:58:55 -0400

    added zkvp

---
+++ zip_kv_pairs.py
@@ -0,0 +1,5 @@
+# ============================================================================================================================
+# Generate dict of sheet1 column ids matched with equivalent sheet2 column ids, stored as {sheet1 column id: sheet2 column id}
+# ============================================================================================================================
+
+sheet1_sheet2_columnIDs_combined = dict(zip(sheet1_columnIDs_from.values(), sheet2_columnIDs_to.values()))
